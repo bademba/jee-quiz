d5ae80ccde6b1e506ebb7fe021e22ff0773f09db
Victor Mwenda <vmwenda.vm@gmail.com>
2017-03-14 19:27:50 +0300

    Added springfox-swagger-ui

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,4 +1,4 @@
-package com.safinterview.net;
+package com;
 
 import com.safaricom.psd.ws.GetKey;
 import com.safaricom.psd.ws.TestWS;
@@ -6,6 +6,8 @@ import com.safaricom.psd.ws.TestWS_Service;
 import com.safaricom.psd.ws.TransactionResult;
 import com.safinterview.io.reader.FileStreamReader;
 import com.safinterview.io.writer.FileStreamWriter;
+import io.swagger.api.AddEmployeeApiController;
+import io.swagger.configuration.HomeController;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Endpoint;
@@ -23,14 +25,15 @@ public class Main {
     private static String filename = "C:\\Users\\victor\\Desktop\\SafInterview\\hidden_key.txt";
 
     public static void main(String[] args) {
-        consumeGetKey();
-        consumeRegister();
+        //consumeGetKey();
+        //consumeRegister();
+
     }
 
     private static void consumeGetKey() {
         try {
             URL url = new URL("http://2a1ba6a5.ngrok.io/TestWebService/TestWS?wsdl");
-            QName qname = new QName("http://ws.psd.safaricom.com/","TestWS");
+            QName qname = new QName("http://ws.psd.safaricom.com/", "TestWS");
             Service service = Service.create(url, qname);
             TestWS testWS = service.getPort(TestWS.class);
             TransactionResult transactionResult = testWS.getKey("red");
@@ -57,7 +60,7 @@ public class Main {
         try {
             String hiddenKey = new FileStreamReader().readFile(filename);
             URL url = new URL("http://2a1ba6a5.ngrok.io/TestWebService/TestWS?wsdl");
-            QName qname = new QName("http://ws.psd.safaricom.com/","TestWS");
+            QName qname = new QName("http://ws.psd.safaricom.com/", "TestWS");
             Service service = Service.create(url, qname);
             TestWS testWS = service.getPort(TestWS.class);
 
